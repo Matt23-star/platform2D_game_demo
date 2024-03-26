c543e8595ab0e14e5170df0baab07eb86046c298
Hao Feng <matthaofeng@gmail.com>
2024-03-26 04:51:41 -0700

    Add block check Color function
    Block gets the same visibility of platform and player

--- Beta/Assets/scripts/EnvironmentController.cs
+++ Beta/Assets/scripts/EnvironmentController.cs
@@ -7,73 +7,85 @@ using UnityEngine.UIElements;
 
 public class EnvironmentController : MonoBehaviour
 {
+    public Tilemap tilemap_white;
+    public Tilemap tilemap_black;
+    public Tilemap tilemap_gray;
+
+    public LayerMask white_ground;
+    public LayerMask black_ground;
+    public LayerMask gray_ground;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckPlayerColor();
+        CheckColor();
+    }
+
+    void CheckPlayerColor()
+    {
+        BoxCollider2D boxCollider = GameObject.Find("Player").GetComponent<BoxCollider2D>();
+        CapsuleCollider2D capsuleCollider = GameObject.Find("Player").GetComponent<CapsuleCollider2D>();
+        Color color = GetComponent<SpriteRenderer>().color;
+
+        if (color == GameObject.Find("Player").GetComponent<SpriteRenderer>().color)
+        {
+            Physics2D.IgnoreCollision(boxCollider, GetComponent<BoxCollider2D>(), true);
+            Physics2D.IgnoreCollision(capsuleCollider, GetComponent<BoxCollider2D>(), true);
+        }
+        else
+        {
+            Physics2D.IgnoreCollision(boxCollider, GetComponent<BoxCollider2D>(), false);
+            Physics2D.IgnoreCollision(capsuleCollider, GetComponent<BoxCollider2D>(), false);
+        }
     }
 
     void CheckColor()
     {
         BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
-        CapsuleCollider2D capsuleCollider = GetComponent<CapsuleCollider2D>();
-        Color color = GetComponent<SpriteRenderer>().color;
 
-        if (color != Color.white )
+        if (GetComponent<SpriteRenderer>().color != Color.white)
         {
             Physics2D.IgnoreCollision(boxCollider, tilemap_black.GetComponent<TilemapCollider2D>(), true);
-            Physics2D.IgnoreCollision(capsuleCollider, tilemap_black.GetComponent<TilemapCollider2D>(), true);
             if (!CheckOverlap(white_ground))
             {
                 Physics2D.IgnoreCollision(boxCollider, tilemap_white.GetComponent<TilemapCollider2D>(), false);
-                Physics2D.IgnoreCollision(capsuleCollider, tilemap_white.GetComponent<TilemapCollider2D>(), false);
             }
 
             if (Physics2D.IsTouching(boxCollider, tilemap_white.GetComponent<TilemapCollider2D>()))
             {
                 Physics2D.IgnoreCollision(boxCollider, tilemap_white.GetComponent<TilemapCollider2D>(), false);
-                Physics2D.IgnoreCollision(capsuleCollider, tilemap_white.GetComponent<TilemapCollider2D>(), false);
             }
         }
         else
         {
             Physics2D.IgnoreCollision(boxCollider, tilemap_white.GetComponent<TilemapCollider2D>(), true);
-            Physics2D.IgnoreCollision(capsuleCollider, tilemap_white.GetComponent<TilemapCollider2D>(), true);
-
             if (!CheckOverlap(black_ground))
             {
                 Physics2D.IgnoreCollision(boxCollider, tilemap_black.GetComponent<TilemapCollider2D>(), false);
-                Physics2D.IgnoreCollision(capsuleCollider, tilemap_black.GetComponent<TilemapCollider2D>(), false);
             }
             if (Physics2D.IsTouching(boxCollider, tilemap_black.GetComponent<TilemapCollider2D>()))
             {
                 Physics2D.IgnoreCollision(boxCollider, tilemap_black.GetComponent<TilemapCollider2D>(), false);
-                Physics2D.IgnoreCollision(capsuleCollider, tilemap_black.GetComponent<TilemapCollider2D>(), false);
             }
         }
     }
 
-
     bool CheckOverlap(LayerMask ground)
     {
         BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
-        CapsuleCollider2D capsuleCollider = GetComponent<CapsuleCollider2D>();
-        if (boxCollider == null || capsuleCollider == null) return false;
+        if (boxCollider == null) return false;
 
-        CapsuleDirection2D direction = capsuleCollider.direction;
 
         float angle = transform.eulerAngles.z;
         // Adjust the position if needed
         Collider2D overlapCollider1 = Physics2D.OverlapBox(boxCollider.bounds.center, boxCollider.size, angle, ground);
-        //find bug. the position lower than observed postion
-        Collider2D overlapCollider2 = Physics2D.OverlapCapsule(capsuleCollider.bounds.center, capsuleCollider.size, direction, angle, ground);
-        if (overlapCollider1 == null && overlapCollider2 == null) return false;
+        if (overlapCollider1 == null) return false;
         return true;
     }
 }
--- Beta/Assets/scripts/bulletFire.cs
+++ Beta/Assets/scripts/bulletFire.cs
@@ -7,13 +7,13 @@ public class BulletFire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,9 +23,10 @@ public class BulletFire : MonoBehaviour
             if (collision.gameObject.GetComponent<SpriteRenderer>().color == Color.black)
             {
                 if (this.gameObject.GetComponent<SpriteRenderer>().color == Color.white)
-                {   
+                {
                     collision.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
                 }
+                Destroy(this.gameObject);
             }
             else if (collision.gameObject.GetComponent<SpriteRenderer>().color == Color.white)
             {
@@ -33,8 +34,35 @@ public class BulletFire : MonoBehaviour
                 {
                     collision.gameObject.GetComponent<SpriteRenderer>().color = Color.black;
                 }
+                Destroy(this.gameObject);
             }
+            //else if (collision.gameObject.GetComponent<SpriteRenderer>().color == Color.gray)
+            //{
+            //    Check if the collision is with a slope
+            //    if (collision.gameObject.GetComponent<Slope>() != null)
+            //    {
+            //        Calculate the reflection direction
+            //        Vector2 incomingDirection = this.gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
+            //        Vector2 normal = collision.contacts[0].normal;
+            //        Vector2 slopeDirection = collision.gameObject.GetComponent<Slope>().slopeDirection;
+            //        Vector2 reflectionDirection = Vector2.Reflect(incomingDirection, slopeDirection);
+
+            //        Set the new direction for the bullet
+
+            //       this.gameObject.GetComponent<Rigidbody2D>().velocity = reflectionDirection * 10f;
+            //    }
+            //    else
+            //    {
+            //        Calculate the reflection direction
+            //        Vector2 incomingDirection = this.gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
+            //        Vector2 normal = collision.contacts[0].normal;
+            //        Vector2 reflectionDirection = Vector2.Reflect(incomingDirection, normal);
+
+            //        Set the new direction for the bullet
+
+            //       this.gameObject.GetComponent<Rigidbody2D>().velocity = reflectionDirection * 10f;
+            //    }
+            //}
         }
-        Destroy(this.gameObject);
     }
 }
